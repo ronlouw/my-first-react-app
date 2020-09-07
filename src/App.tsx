fd65c59ee94acdaa75a9b68b9ccb90faa0e53122
Ronald van der Louw <rvanderlouw@conclusion.nl>
2020-09-07 12:21:53 +0200

    Added new components

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React training
+          React aanpassingen
         </a>
       </header>
     </div>
