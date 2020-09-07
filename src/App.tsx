767d6c41c17e0e480b075b6d3ef4126ede0ccd7a
Ronald van der Louw <rvanderlouw@conclusion.nl>
2020-09-07 11:55:05 +0200

    change welcome text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Hello World
+          Welcome to the react training
         </a>
       </header>
     </div>
